4ec007af703fa2e8c03275c3450b29dbac07d390
Taha <TahaM8000@gmail.com>
2024-06-27 20:40:36 +0330

    preorder traversal implementation

--- ast_code/ast.py
+++ ast_code/ast.py
@@ -3,11 +3,20 @@ class AST:
         self.root = None
         self.current_number = 0
 
+    def get_pre_order(self):
+        self.pre_order(self.root)
+
+    def pre_order(self,node):
+        print(node.value)
+        for child in node.children:
+            self.pre_order(child)
+
     class TreeNode:
         def __init__(self, value, children, number):
             self.value = value
             self.children = children
             self.number = number
+
 
     def make_node(self, value, children):
         tree_node = self.TreeNode(value, children, self.current_number)
